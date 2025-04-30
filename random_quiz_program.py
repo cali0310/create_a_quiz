2e93d8708251022eda76c2f80232e8d2613fac95
Cali0310 <ramosyazmien2@gmail.com>
2025-04-30 18:42:08 +0800

    adding widgets in GUI

--- random_quiz_program.py
+++ random_quiz_program.py
@@ -36,6 +36,25 @@ class quiz_app:
         self.create_widgets()
         self.display_question()
 
+    def create_widgets(self):
+        self.score_label=tk.Label(self.master, text="Score: 0", font=("Helvetica", 12, "bold"))
+        self.score_label.pack(pady=10)
+        self.question_label = tk.Label(self.master, textvariable=self.question_var, font=("Helvetica", 14), wraplength=450)
+        self.question_label.pack(pady=20)
+
+        self.option_buttons = {}
+        for opt in ['a', 'b', 'c', 'd']:
+            rb = tk.Radiobutton(self.master, text="", variable=self.selected_option, value=opt,
+                                font=("Helvetica", 12), wraplength=450, anchor="w", justify="left")
+            rb.pack(anchor="w", padx=40, pady=2)
+            self.option_buttons[opt] = rb
+
+        self.submit_button = tk.Button(self.master, text="Submit", command=self.check_answer, font=("Helvetica", 12))
+        self.submit_button.pack(pady=10)
+
+        self.exit_button = tk.Button(self.master, text="Exit", command=self.exit_quiz, font=("Helvetica", 12), fg="red")
+        self.exit_button.pack(pady=5)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
